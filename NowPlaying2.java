4fb62acf800c4cfa43b6fe9fe5733cdd3d6650a2
brittanycaesar2 <38293211+brittanycaesar2@users.noreply.github.com>
2018-06-24 21:13:38 -0700

    Add files via upload

---
+++ NowPlaying2.java
@@ -0,0 +1,28 @@
+package com.example.android.music;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class NowPlaying2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
